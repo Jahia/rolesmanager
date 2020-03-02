83ff50fe1657b91fac5a92b05bce6b6ce4a86e0d
cedmail <cmailleux@jahia.com>
2020-03-02 12:57:15 -0500

    BACKLOG-11710: Add requiredPermission into routes

--- src/javascript/registrations/rolesAndPermissions/registerRoutes.js
+++ src/javascript/registrations/rolesAndPermissions/registerRoutes.js
@@ -12,6 +12,7 @@ export const registerRoutes = function () {
         path: path,
         route: route,
         defaultPath: path,
+        requiredPermission: 'adminRoles',
         icon: null,
         label: 'rolesmanager:rolesAndPermissions.label',
         childrenTarget: 'usersandroles',
