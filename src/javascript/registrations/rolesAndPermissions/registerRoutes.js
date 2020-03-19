f53bce951a35f70fcf2837591c5554d05e390e7d
Thomas Draier <tdraier@jahia.com>
2020-03-19 14:34:07 +0100

    BACKLOG-12820: Adapted adminRoute format

--- src/javascript/registrations/rolesAndPermissions/registerRoutes.js
+++ src/javascript/registrations/rolesAndPermissions/registerRoutes.js
@@ -1,22 +1,12 @@
 import {registry} from '@jahia/ui-extender';
 
 export const registerRoutes = function () {
-    const level = 'server';
-    const parentTarget = 'administration-server';
-
-    const path = '/administration/rolesAndPermissions';
-    const route = 'rolesAndPermissions';
-    registry.addOrReplace('adminRoute', `${level}-${path.toLowerCase()}`, {
-        id: route,
-        targets: [`${parentTarget}-usersandroles:6`],
-        path: path,
-        route: route,
-        defaultPath: path,
+    registry.addOrReplace('adminRoute', 'rolesAndPermissions', {
+        targets: ['administration-server-usersAndRoles:6'],
         requiredPermission: 'adminRoles',
         icon: null,
         label: 'rolesmanager:rolesAndPermissions.label',
-        childrenTarget: 'usersandroles',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/settings.rolesAndPermissions.html'
     });
 };
